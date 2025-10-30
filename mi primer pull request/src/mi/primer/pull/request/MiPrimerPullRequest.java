59c9a9c95338219916b93ff62a8c03a25af0858d
andresjimenez6 <andrescortijo2006@gmail.com>
2025-10-30 12:28:40 +0100

    primer pull request

---
+++ mi primer pull request/src/mi/primer/pull/request/MiPrimerPullRequest.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mi.primer.pull.request;
+import java.util.Scanner;
+/**
+ *
+ * @author EAG
+ */
+public class MiPrimerPullRequest {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Dime un numero del 1 - 10");
+        int num = sc.nextInt();
+        System.out.println("gilipollas");
+    }
+    
+}
